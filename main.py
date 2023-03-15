8af862e6ab21a05439fe8215d08be0ecacc22121
Matthew Jin <mjin.code@gmail.com>
2023-03-15 15:30:04 -0400

    Add normal equation solutions and random feature models

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import os
 import sys
 import pickle
 from dataset import *
-from model import NonlinearNN
+from model import *
 from trainer import Trainer
 from transforms import *
 from utils import *
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torch.optim as optim
 
 import numpy as np
@@ -16,7 +17,48 @@ import matplotlib.pyplot as plt
 
 savepath = "/Users/matthewjin/imajin/correlated-noise/checkpoints"
 
-def run(train_dataset, test_dataset, logname, logdir, in_dim=64,
+def run_rfa(feature_dim, init_var, relu, train_dataset, test_dataset, transform,
+            logname, logdir, runs=100):
+    trainer = Trainer(train_dataset, test_dataset)
+    logs, models = [], []
+    criterion = nn.MSELoss(reduction='sum')
+    X = torch.clone(train_dataset.data)
+    y = F.one_hot(train_dataset.classes, num_classes=10)
+    for i in range(runs):
+        model = RandomFeatureModel(feature_dim, init_var, relu=relu)
+        fX = model.features(X)
+        fX_b = torch.ones(fX.size(0), fX.size(1)+1)
+        fX_b[:, :fX.size(1)] = fX
+        theta = torch.linalg.pinv(fX_b.T @ fX_b) @ fX_b.T @ y.float()
+        model.linear.weight = nn.Parameter(theta[:feature_dim, :].T,
+                                           requires_grad=False)
+        model.linear.bias = nn.Parameter(theta[feature_dim, :],
+                                         requires_grad=False)
+        """Logging"""
+        logger = ListLogger()
+        l0, a0, cl0, ca0 = trainer.eval(model, train_dataset, criterion)
+        l1, a1, cl1, ca1 = trainer.eval(model, test_dataset, criterion)
+        logger.log('train_loss', v=l0)
+        logger.log('train_acc', v=a0)
+        logger.log('test_loss', v=l1)
+        logger.log('test_acc', v=a1)
+        for c in range(10):
+            logger.log('train_class_loss', c, v=cl0[c])
+            logger.log('train_class_acc', c, v=ca0[c])
+            logger.log('test_class_loss', c, v=cl1[c])
+            logger.log('test_class_acc', c, v=ca1[c])
+        logs.append(logger)
+        models.append(model.feature.data)
+        """"""
+
+    logs = collect(logs)
+    with open(os.path.join(savepath, logdir, logname + "_logs.pkl"), 'wb') as f:
+        pickle.dump(logs, f)
+    with open(os.path.join(savepath, logdir, logname + "_models.pkl"), 'wb') as f:
+        pickle.dump(models, f)
+
+
+def run_gradient(train_dataset, test_dataset, logname, logdir, in_dim=64,
               hidden_dim=32, transform=None, runs=10, epochs=100):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -40,6 +82,7 @@ def run(train_dataset, test_dataset, logname, logdir, in_dim=64,
     with open(os.path.join(savepath, logdir, logname + ".pkl"), 'wb') as f:
         pickle.dump(col, f)
 
+
 def run_optdigits(logname, logdir, params, runs=100, epochs=100):
     datapath = "/Users/matthewjin/imajin/correlated-noise/optdigits/"
     trainpath = os.path.join(datapath, "optdigits.tra")
@@ -55,11 +98,15 @@ def run_optdigits(logname, logdir, params, runs=100, epochs=100):
 
     for i in range(len(params)):
         print(f"Run {i}: p={params[i]}")
-        transform = None
+        # transform = None
         # transform = MultivariateNormalNoise(torch.zeros(64), params[i]*cov)
-        run(train_dataset, test_dataset, logname=f"{logname}_{params[i]}",
-            logdir=logdir, transform=transform, runs=runs, epochs=epochs,)
-            # hidden_dim=params[i])
+        # run_gradient(train_dataset, test_dataset,
+        #              logname=f"{logname}_{params[i]}", logdir=logdir,
+        #              transform=transform, runs=runs, epochs=epochs,)
+        run_rfa(params[i], 1, True, train_dataset, test_dataset,
+                transform=None, logname=f"{logname}_{params[i]}", logdir=logdir,
+                runs=runs)
+
 
 def run_mnist(logname, logdir, params, runs=10, epochs=250):
     datapath = "/Users/matthewjin/imajin/correlated-noise/mnist-back-image/"
@@ -70,8 +117,9 @@ def run_mnist(logname, logdir, params, runs=10, epochs=250):
     for i in range(len(params)):
         print(f"Run {i}: p={params[i]}")
         transform = None
-        run(train_dataset, test_dataset, logname=f"{logname}_{params[i]}",
-            logdir=logdir, transform=transform, runs=runs, epochs=epochs)
+        run_gradient(train_dataset, test_dataset,
+                     logname=f"{logname}_{params[i]}", logdir=logdir,
+                     transform=transform, runs=runs, epochs=epochs)
 
 
 def main():
@@ -82,27 +130,11 @@ def main():
     # params = [0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90]
     # params = [0.01, 0.05, 1.00, 1.20, 1.40, 1.60, 1.80, 2.00]
     """Hidden dims"""
-    # params = [4, 8, 12, 16, 20, 24, 28, 32, 36, 40]
-    # params = [44, 48, 52, 56, 60, 64]
-    # params = [5, 6, 7]
+    params = [8, 16, 32, 64, 80, 96, 112, 128, 160, 192, 224, 256, 320, 384,
+              448, 512]
 
-    run_optdigits(logname=f"opt_base_linear_ce", logdir=f".", params=params,
-                  epochs=250, runs=25)
-
-    ## Example augmentations
-    """
-    # transform = None
-    # transform = MultivariateNormalNoise(torch.zeros(64), params[i]*torch.eye(64))
-    # transform = MultivariateNormalNoise(torch.zeros(784),
-    #                                     params[i]*torch.eye(784))
-    # transform = Mask(p=params[i])
-    # transform = SparseNoise(m=1, p=params[i])
-    # transform = SaltAndPepper(p=params[i])
-    # transform = ClassTransform(
-    #         {9: MultivariateNormalNoise(torch.zeros(64),
-    #                                     params[i]*torch.eye(64)),
-    #         })
-    """
+    run_optdigits(logname=f"rfa", logdir=f"rfa_relu_fdim_sweep", params=params,
+                  epochs=100, runs=100)
 
 if __name__ == "__main__":
     main()
--- model.py
+++ model.py
@@ -33,3 +33,25 @@ class NonlinearNN(nn.Module):
     def forward(self, x):
         """Returns the result of the forward pass of the neural network."""
         return self.linear2(self.relu(self.linear1(x)))
+
+
+class RandomFeatureModel(nn.Module):
+    def __init__(self, feature_dim, init_var, in_dim=64, out_dim=10, relu=True):
+        super(RandomFeatureModel, self).__init__()
+
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+        self.feature_dim = feature_dim
+
+        projection_matrix = torch.normal(torch.zeros(in_dim, feature_dim),
+                                         torch.ones(in_dim, feature_dim) *
+                                         np.sqrt(init_var))
+        self.feature = nn.Parameter(projection_matrix)
+        self.relu = nn.ReLU() if relu else lambda x: x
+        self.linear = nn.Linear(feature_dim, out_dim)
+
+    def features(self, x):
+        return self.relu(x @ self.feature)
+
+    def forward(self, x):
+        return self.linear(self.relu(x @ self.feature))
